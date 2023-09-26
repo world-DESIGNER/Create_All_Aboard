5b23a6e3f6fc766283046d34e9b2c3459b68acde
gisellevonbingen <gisellevonbingen@gmail.com>
2023-09-26 14:09:41 +0900

    Steam Engine can be place anywhere

---
+++ src/main/java/steve_gall/create_trainwrecked/common/mixin/SteamEngineBlockMixin.java
@@ -0,0 +1,26 @@
+package steve_gall.create_trainwrecked.common.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+import com.simibubi.create.content.kinetics.steamEngine.SteamEngineBlock;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.FaceAttachedHorizontalDirectionalBlock;
+import net.minecraft.world.level.block.state.BlockState;
+
+@Mixin(value = SteamEngineBlock.class)
+public abstract class SteamEngineBlockMixin extends FaceAttachedHorizontalDirectionalBlock
+{
+	public SteamEngineBlockMixin(Properties pProperties)
+	{
+		super(pProperties);
+	}
+
+	@Override
+	public boolean canSurvive(BlockState pState, LevelReader pLevel, BlockPos pPos)
+	{
+		return super.canSurvive(pState, pLevel, pPos);
+	}
+
+}

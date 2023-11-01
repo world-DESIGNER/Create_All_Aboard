348e84339751ad2b6911708be32974913b3d7fb9
gisellevonbingen <gisellevonbingen@gmail.com>
2023-11-01 15:53:53 +0900

    TrainSteamEngine can bind shaft

--- src/main/java/steve_gall/create_all_aboard/common/block/TrainSteamEngineBlock.java
+++ src/main/java/steve_gall/create_all_aboard/common/block/TrainSteamEngineBlock.java
@@ -2,12 +2,32 @@ package steve_gall.create_all_aboard.common.block;
 
 import static net.minecraft.world.level.block.state.properties.BlockStateProperties.WATERLOGGED;
 
+import java.util.function.Predicate;
+
+import com.simibubi.create.AllBlocks;
 import com.simibubi.create.AllShapes;
 import com.simibubi.create.content.equipment.wrench.IWrenchable;
+import com.simibubi.create.content.fluids.tank.FluidTankBlock;
+import com.simibubi.create.content.kinetics.simpleRelays.ShaftBlock;
+import com.simibubi.create.content.kinetics.steamEngine.PoweredShaftBlock;
+import com.simibubi.create.content.kinetics.steamEngine.SteamEngineBlock;
+import com.simibubi.create.foundation.advancement.AdvancementBehaviour;
 import com.simibubi.create.foundation.block.IBE;
+import com.simibubi.create.foundation.placement.IPlacementHelper;
+import com.simibubi.create.foundation.placement.PlacementHelpers;
+import com.simibubi.create.foundation.placement.PlacementOffset;
+import com.simibubi.create.foundation.utility.BlockHelper;
 
+import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.core.Direction.Axis;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
@@ -22,6 +42,7 @@ import net.minecraft.world.level.block.state.properties.AttachFace;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.level.pathfinder.PathComputationType;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import steve_gall.create_all_aboard.common.block.entity.TrainSteamEngineBlockEntity;
@@ -29,6 +50,8 @@ import steve_gall.create_all_aboard.common.init.ModBlockEntityTypes;
 
 public class TrainSteamEngineBlock extends FaceAttachedHorizontalDirectionalBlock implements SimpleWaterloggedBlock, IWrenchable, IBE<TrainSteamEngineBlockEntity>
 {
+	private static final int placementHelperId = PlacementHelpers.register(new PlacementHelper());
+
 	public TrainSteamEngineBlock(Properties pProperties)
 	{
 		super(pProperties);
@@ -45,6 +68,17 @@ public class TrainSteamEngineBlock extends FaceAttachedHorizontalDirectionalBloc
 	public FluidState getFluidState(BlockState state)
 	{
 		return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : Fluids.EMPTY.defaultFluidState();
+	}
+
+	@Override
+	public InteractionResult use(BlockState state, Level world, BlockPos pos, Player player, InteractionHand hand, BlockHitResult ray)
+	{
+		ItemStack heldItem = player.getItemInHand(hand);
+
+		IPlacementHelper placementHelper = PlacementHelpers.get(placementHelperId);
+		if (placementHelper.matchesItem(heldItem))
+			return placementHelper.getOffset(player, world, state, pos, ray).placeInWorld(world, (BlockItem) heldItem.getItem(), player, hand, ray);
+		return InteractionResult.PASS;
 	}
 
 	@Override
@@ -89,6 +123,35 @@ public class TrainSteamEngineBlock extends FaceAttachedHorizontalDirectionalBloc
 	}
 
 	@Override
+	public void onPlace(BlockState pState, Level pLevel, BlockPos pPos, BlockState pOldState, boolean pIsMoving)
+	{
+		FluidTankBlock.updateBoilerState(pState, pLevel, pPos.relative(SteamEngineBlock.getFacing(pState).getOpposite()));
+		BlockPos shaftPos = SteamEngineBlock.getShaftPos(pState, pPos);
+		BlockState shaftState = pLevel.getBlockState(shaftPos);
+		if (SteamEngineBlock.isShaftValid(pState, shaftState))
+			pLevel.setBlock(shaftPos, PoweredShaftBlock.getEquivalent(shaftState), 3);
+	}
+
+	@Override
+	public void onRemove(BlockState pState, Level pLevel, BlockPos pPos, BlockState pNewState, boolean pIsMoving)
+	{
+		if (pState.hasBlockEntity() && (!pState.is(pNewState.getBlock()) || !pNewState.hasBlockEntity()))
+			pLevel.removeBlockEntity(pPos);
+		FluidTankBlock.updateBoilerState(pState, pLevel, pPos.relative(SteamEngineBlock.getFacing(pState).getOpposite()));
+		BlockPos shaftPos = SteamEngineBlock.getShaftPos(pState, pPos);
+		BlockState shaftState = pLevel.getBlockState(shaftPos);
+		if (AllBlocks.POWERED_SHAFT.has(shaftState))
+			pLevel.scheduleTick(shaftPos, shaftState.getBlock(), 1);
+	}
+
+	@Override
+	public void setPlacedBy(Level pLevel, BlockPos pPos, BlockState pState, LivingEntity pPlacer, ItemStack pStack)
+	{
+		super.setPlacedBy(pLevel, pPos, pState, pPlacer, pStack);
+		AdvancementBehaviour.setPlacedBy(pLevel, pPos, pPlacer);
+	}
+
+	@Override
 	public Class<TrainSteamEngineBlockEntity> getBlockEntityClass()
 	{
 		return TrainSteamEngineBlockEntity.class;
@@ -98,6 +161,42 @@ public class TrainSteamEngineBlock extends FaceAttachedHorizontalDirectionalBloc
 	public BlockEntityType<? extends TrainSteamEngineBlockEntity> getBlockEntityType()
 	{
 		return ModBlockEntityTypes.TRAIN_STEAM_ENGINE.get();
+	}
+
+	@MethodsReturnNonnullByDefault
+	private static class PlacementHelper implements IPlacementHelper
+	{
+		@Override
+		public Predicate<ItemStack> getItemPredicate()
+		{
+			return AllBlocks.SHAFT::isIn;
+		}
+
+		@Override
+		public Predicate<BlockState> getStatePredicate()
+		{
+			return s -> s.getBlock() instanceof TrainSteamEngineBlock;
+		}
+
+		@Override
+		public PlacementOffset getOffset(Player player, Level world, BlockState state, BlockPos pos, BlockHitResult ray)
+		{
+			BlockPos shaftPos = SteamEngineBlock.getShaftPos(state, pos);
+			BlockState shaft = AllBlocks.SHAFT.getDefaultState();
+			for (Direction direction : Direction.orderedByNearest(player))
+			{
+				shaft = shaft.setValue(ShaftBlock.AXIS, direction.getAxis());
+				if (SteamEngineBlock.isShaftValid(state, shaft))
+					break;
+			}
+
+			BlockState newState = world.getBlockState(shaftPos);
+			if (!newState.getMaterial().isReplaceable())
+				return PlacementOffset.fail();
+
+			Axis axis = shaft.getValue(ShaftBlock.AXIS);
+			return PlacementOffset.success(shaftPos, s -> BlockHelper.copyProperties(s, AllBlocks.POWERED_SHAFT.getDefaultState()).setValue(PoweredShaftBlock.AXIS, axis));
+		}
 	}
 
 }
--- src/main/java/steve_gall/create_all_aboard/common/block/entity/TrainSteamEngineBlockEntity.java
+++ src/main/java/steve_gall/create_all_aboard/common/block/entity/TrainSteamEngineBlockEntity.java
@@ -1,25 +1,119 @@
 package steve_gall.create_all_aboard.common.block.entity;
 
+import java.lang.ref.WeakReference;
 import java.util.List;
 
+import javax.annotation.Nullable;
+
+import com.simibubi.create.content.kinetics.base.KineticBlockEntityRenderer;
+import com.simibubi.create.content.kinetics.steamEngine.PoweredShaftBlockEntity;
+import com.simibubi.create.content.kinetics.steamEngine.SteamEngineBlock;
 import com.simibubi.create.foundation.blockEntity.SmartBlockEntity;
 import com.simibubi.create.foundation.blockEntity.behaviour.BlockEntityBehaviour;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.core.Direction.Axis;
+import net.minecraft.core.Direction.AxisDirection;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.AABB;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import steve_gall.create_all_aboard.common.init.ModBlockEntityTypes;
 
 public class TrainSteamEngineBlockEntity extends SmartBlockEntity
 {
+	public WeakReference<PoweredShaftBlockEntity> target;
+
 	public TrainSteamEngineBlockEntity(BlockPos pos, BlockState state)
 	{
 		super(ModBlockEntityTypes.TRAIN_STEAM_ENGINE.get(), pos, state);
+
+		target = new WeakReference<>(null);
 	}
 
 	@Override
 	public void addBehaviours(List<BlockEntityBehaviour> behaviours)
 	{
 
+	}
+
+	@Override
+	public void tick()
+	{
+		super.tick();
+		PoweredShaftBlockEntity shaft = getShaft();
+
+		if (level.isClientSide())
+			return;
+		if (shaft == null)
+			return;
+		if (shaft.enginePos != null && !shaft.getBlockPos().subtract(worldPosition).equals(shaft.enginePos))
+			return;
+		Direction facing = SteamEngineBlock.getFacing(getBlockState());
+		if (level.isLoaded(worldPosition.relative(facing.getOpposite())))
+			shaft.update(worldPosition, 0, 0);
+	}
+
+	@Override
+	public void remove()
+	{
+		PoweredShaftBlockEntity shaft = getShaft();
+		if (shaft != null)
+			shaft.remove(worldPosition);
+		super.remove();
+	}
+
+	@Override
+	@OnlyIn(Dist.CLIENT)
+	protected AABB createRenderBoundingBox()
+	{
+		return super.createRenderBoundingBox().inflate(2);
+	}
+
+	public PoweredShaftBlockEntity getShaft()
+	{
+		PoweredShaftBlockEntity shaft = target.get();
+		if (shaft == null || shaft.isRemoved() || !shaft.canBePoweredBy(worldPosition))
+		{
+			if (shaft != null)
+				target = new WeakReference<>(null);
+			Direction facing = SteamEngineBlock.getFacing(getBlockState());
+			BlockEntity anyShaftAt = level.getBlockEntity(worldPosition.relative(facing, 2));
+			if (anyShaftAt instanceof PoweredShaftBlockEntity ps && ps.canBePoweredBy(worldPosition))
+				target = new WeakReference<>(shaft = ps);
+		}
+		return shaft;
+	}
+
+	@Nullable
+	@OnlyIn(Dist.CLIENT)
+	public Float getTargetAngle()
+	{
+		float angle = 0;
+		BlockState blockState = getBlockState();
+		if (!ModBlockEntityTypes.TRAIN_STEAM_ENGINE.get().isValid(blockState))
+			return null;
+
+		Direction facing = SteamEngineBlock.getFacing(blockState);
+		PoweredShaftBlockEntity shaft = getShaft();
+		Axis facingAxis = facing.getAxis();
+		Axis axis = Axis.Y;
+
+		if (shaft == null)
+			return null;
+
+		axis = KineticBlockEntityRenderer.getRotationAxisOf(shaft);
+		angle = KineticBlockEntityRenderer.getAngleForTe(shaft, shaft.getBlockPos(), axis);
+
+		if (axis == facingAxis)
+			return null;
+		if (axis.isHorizontal() && (facingAxis == Axis.X ^ facing.getAxisDirection() == AxisDirection.POSITIVE))
+			angle *= -1;
+		if (axis == Axis.X && facing == Direction.DOWN)
+			angle *= -1;
+		return angle;
 	}
 
 }
